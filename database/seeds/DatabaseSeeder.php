487f0c8138526301500779dcb6bac359e5b43bbf
MinThiha <51397647+minthiha-ai@users.noreply.github.com>
2024-08-01 15:58:19 +0700

    User Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'Thuma4future',
+            'email' => 'thuma@admin.com',
+            'password' => bcrypt('Thum@4Futur3')
+        ]);
     }
 }
